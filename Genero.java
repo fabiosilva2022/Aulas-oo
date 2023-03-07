a44cf201b9fe3740b22aa511185562c5cdb31cb5
Fabio <f.luiz.10.1@gmail.com>
2023-03-06 21:49:33 -0400

    Aula de Orientações a objetos

---
+++ Genero.java
@@ -0,0 +1,14 @@
+public class Genero {
+
+    public String descricao;
+
+    public String getDescricao(){
+        return this.descricao;
+
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    
+}
--- Principal.java
+++ Principal.java
@@ -9,12 +9,20 @@ public class Principal {
         filme1.setDuracao(122);
         filme1.setAno(2023);
         filme1.setSinopse("O Camelo que ataca as pessoas...");
+        
 
 
         System.out.println(filme1.getTitulacao());
         System.out.println(filme1.getDuracao());
         System.out.println(filme1.getAno());
         System.out.println(filme1.getSinopse());
+
+        Genero genero1 = new Genero();
+        genero1.setDescricao("Teror");
+        
+        System.out.println(genero1.getDescricao());
+
+
 
     }
     
